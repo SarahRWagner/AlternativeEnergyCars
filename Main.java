43ac011aae5e6f52051a271b129925e2f866737e
Sarah Wagner <swagner0334@students.mc3.edu>
2019-03-21 22:34:33 -0400

    Main Method Addition
    Added objects to main method to show inheritance

--- Main.java
+++ Main.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+
 public class Main {
+
+    public static void main(String[] args){
+
+        AlternativeEnergyCar kiaNiro = new PHEVElectricCar(2018, "Kia", 25.0, "White", 1.0, 1, 1.56, "Optima", 2.5);
+        AlternativeEnergyCar hyundaiNexo = new HydrogenCar(2019, "Hyundai", 25.0, "blue", "Hydrogen", 80.0, 70.0);
+        AlternativeEnergyCar stanleySteamer = new SteamCar(1912, "Stanley Motor Carriage Company", 30.0, "Green", "Steam",40.0, "Gas" );
+
+
+        ArrayList<AlternativeEnergyCar> carCollection = new ArrayList<>();
+        carCollection.add(kiaNiro);
+        carCollection.add(hyundaiNexo);
+        carCollection.add(stanleySteamer);
+
+    }
 }
